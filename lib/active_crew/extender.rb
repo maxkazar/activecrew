a9aaddb5dd60f327f701a48bba801b34d0350497
Max Kazarin <maxkazargm@gmail.com>
2017-03-21 09:24:15 +0300

    exec command without options

--- lib/active_crew/extender.rb
+++ lib/active_crew/extender.rb
@@ -1,6 +1,6 @@
 module ActiveCrew
   module Extender
-    def command(name, options)
+    def command(name, options = {})
       context = { options: options, session: request.headers['x-session-token'] }
 
       ActiveCrew::Backends.enqueue name, current_user, context
